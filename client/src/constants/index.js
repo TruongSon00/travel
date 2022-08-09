888987e36d26dc687e90641c22de7282561c0e91
truongson00 <soi30082000@gmail.com>
2022-08-09 09:48:07 +0700

    hihi

--- client/src/constants/index.js
+++ client/src/constants/index.js
@@ -1,2 +1,2 @@
-const host = process.env.host || 'https://travel-3008.herokuapp.com'
+const host = process.env.REACT_APP_HOST || 'https://travel-3008.herokuapp.com'
 export const URL_API = `${host}/api`;
